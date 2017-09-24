79e5783e380420b2b84c11c07634fb3535c12a17
kiranvanteru <kk23vanteru@gmail.com>
2017-09-24 20:06:10 +0530

    Bar1

---
+++ baronetest/src/main/java/com/baronePgae/Bar.java
@@ -0,0 +1,11 @@
+package com.baronePgae;
+
+import org.testng.annotations.Test;
+
+public class Bar {
+  @Test
+  public void fun() {
+	  
+	  System.out.println("we are at bar one ameerpet");
+  }
+}
